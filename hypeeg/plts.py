eea1f560c830fdff59c9a6bf0198e9136de14654
TomDonoghue <tdonoghue@ucsd.edu>
2018-08-07 12:10:21 -0400

    Add plts file

---
+++ hypeeg/plts.py
@@ -0,0 +1,56 @@
+"""  """
+import numpy as np
+import matplotlib.pyplot as plt
+
+##
+##
+
+def plot_results(times, results, labels):
+    """   """
+
+    fig, ax = plt.subplots(figsize=(14, 6))
+
+    for result in results:
+        plot_time_class(times, np.mean(result, 0), sem(result), ax)
+
+    # Chance line @ 50%
+    plot_time_class(times, np.ones(len(times)) * 0.5, ax=ax, color='grey', alpha=0.7)
+
+    ax.set_xlim([-1, 1])
+    ax.set_ylim([0.4, 0.9])
+
+    ax.set_xlabel('Time (s)', fontsize=14)
+    ax.set_ylabel('Classification Accuracy (%)', fontsize=14)
+
+    ## Aesthetics
+
+    #ax.grid(True)
+
+    # Set tick fontsizes
+    plt.setp(ax.get_xticklabels(), fontsize=12)
+    plt.setp(ax.get_yticklabels(), fontsize=12)
+
+    # Set the top and right side frame & ticks off
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    # Set linewidth of remaining spines
+    ax.spines['left'].set_linewidth(2)
+    ax.spines['bottom'].set_linewidth(2)
+
+    # Add a legend
+    plt.legend(labels, fontsize=14)
+
+
+def plot_time_class(times, dat, err=None, ax=None, *args, **kwargs):
+    """   """
+
+    if not ax:
+        fig, ax = plt.subplots(figsize=(14, 6))
+
+    ax.plot(times, dat, *args, **kwargs)
+
+    if np.any(err):
+        ax.fill_between(times, dat-err, dat+err, alpha=0.5)
